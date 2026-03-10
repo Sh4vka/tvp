6450efd05fd900fa1af57ca3a6006b32114e1999
Titov Matvey <matvey11334422@gmail.com>
2026-03-10 08:25:27 +0300

    fix(first task): 4 задание работает нормально

--- lab1/info_status.cpp
+++ lab1/info_status.cpp
@@ -11,31 +11,8 @@ namespace infoproc {
 template<typename T>
 class GetInfo {
 public:
-    GetInfo(bool createChild = false) : parentPid(static_cast<T>(getpid())) {
-        if (createChild) {
-            T childPid = fork();
-            switch (childPid) {
-                case -1:
-                    std::cerr <<"Fork failed" << std::endl;
-                    isChild = false;
-                    break;
-
-                case 0:
-                    isChild = true;
-                    myPid = parentPid;
-                    break;
-
-                default:
-                    childPid = -1;
-                    isChild = false;
-                    myPid = parentPid;
-            }
-        }
-    }
-
-    bool is_chiled_process() const {return isChild;}
-    T get_current_pid() const {return myPid;}
-
+    GetInfo() {pid = static_cast<T>(getpid());}
+    
     std::string getproc() {
         std::string filename = getfile();
         std::cout << filename << std::endl;
@@ -52,21 +29,13 @@ public:
     }
 
 private:
-    std::string getfile() { return "/proc/" + std::to_string(static_cast<int>(myPid)); }
-    T myPid;
-    T parentPid;
-    bool isChild;
+    std::string getfile() { return "/proc/" + std::to_string(static_cast<int>(pid)) + "/status"; }
+    T pid;
 };
 }
 
 int main() {
-    infoproc::GetInfo<pid_t> proc(true);
-    if (proc.is_chiled_process()) {
-        std::cout << proc.getproc() << std::endl;
-        exit(0);
-    } else {
-        std::cout << proc.getproc() << std::endl;
-        wait(nullptr);
-    }
+    infoproc::GetInfo<pid_t> proc;
+    std::cout << proc.getproc() << std::endl;
     return 0;
 }
